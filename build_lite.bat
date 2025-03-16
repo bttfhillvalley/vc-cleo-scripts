c6e1783683c4f2e8fedaa9a86127e300d8681c26
BTTF Hill Valley <53920904+bttfhillvalley@users.noreply.github.com>
2025-03-16 19:38:42 +0000

    Revert "Update build_lite.bat"
    This reverts commit 8f5addc1d24a380fc9e5e0947cdd3c1c1faf5cd8.

--- build_lite.bat
+++ build_lite.bat
@@ -110,7 +110,6 @@ call :Compile Rogers\Whistle .s
 call :Compile PickupsLite .cs
 call :Compile Pickups\Plutonium .s
 call :Compile Pickups\PlutoniumText .s
-call :Compile Pickups\PlutoniumBox .s
 call :Compile Pickups\FlashHud .s
 
 :: Docs GMC Van/Truck scripts
